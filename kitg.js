46124011e038170ea35c380211e5f77fe01731a9
dandcvs <dand73@mail.ru>
2018-07-31 16:56:22 +0400

    bfix

--- kitg.js
+++ kitg.js
@@ -309,8 +309,8 @@ var resourcesAll = [
     ["kerosene", [["oil",7500]],0],
     ["parchment", [["furs",175]],0],
     ["manuscript", [["parchment",25],["culture",400]],35],
-    ["compedium", [["manuscript",50],["science",10000]],1],
-    ["blueprint", [["compedium",25],["science",25000]],1],
+    ["compedium", [["manuscript",50],["science",10000]],0],
+    ["blueprint", [["compedium",25],["science",25000]],0],
     ["thorium", [["uranium",250]],0],
     ["megalith", [["slab",50],["plate",5]],0],
 ]
@@ -327,9 +327,11 @@ function autoCraft2() {
                 if (gamePage.workshop.getCraft(resourcesAllF[i][0]).unlocked) {
                      flag = true;
                      cnt = 0;
-                     if (curResTarget.value <= Math.min(resourcesAllF[i][2] , gamePage.resPool.get('paragon').value) ) {
+                     if (curResTarget.value <= Math.min(resourcesAllF[i][2] , gamePage.resPool.get('paragon').value)) {
                         if (gamePage.resPool.get(resourcesAllF[i][1][0][0]).value >= resourcesAllF[i][1][0][1]) {
-                            cnt = Math.ceil(gamePage.resPool.get(resourcesAllF[i][1][0][0]).value /resourcesAllF[i][1][0][1]/2);
+                             for (var x = 0; x < resourcesAllF[i][1].length; x++) {
+                                cnt = Math.min(cnt != 0 ? cnt : Math.ceil((gamePage.resPool.get(resourcesAllF[i][1][x][0]).value / resourcesAllF[i][1][x][1]/2),Math.ceil((gamePage.resPool.get(resourcesAllF[i][1][x][0]).value / resourcesAllF[i][1][x][1])/2)));
+                             }
                         }
                      }
                      else{
