19ac3595d4782350f56e83cd21f1f282838dcf1d
dandcvs <dand73@mail.ru>
2018-08-13 09:44:53 +0400

    iw trade griggins fix

--- kitg.js
+++ kitg.js
@@ -288,6 +288,9 @@ function autoTrade() {
                     }
                 } else if (titRes.value < (titRes.maxValue * 0.9)  && gamePage.diplomacy.get('zebras').unlocked) {
                     gamePage.diplomacy.tradeAll(game.diplomacy.get("zebras"));
+                }else if ((mineralsRes.value < (mineralsRes.maxValue * 0.9)) && (ivoryRes.value > mineralsRes.value) && gamePage.diplomacy.get('nagas').unlocked) {
+                    gamePage.diplomacy.tradeAll(game.diplomacy.get("nagas"));
+
                 } else if (((ironRes.value < (ironRes.maxValue * 0.9) && !gamePage.ironWill) || (ironRes.value < (ironRes.maxValue * 0.5) && gamePage.ironWill)) && (woodRes.value > (woodRes.maxValue * 0.8)) && gamePage.diplomacy.get('griffins').unlocked) {
                     if (gamePage.ironWill) {
                         gamePage.diplomacy.tradeMultiple(game.diplomacy.get("griffins"),Math.ceil(gamePage.diplomacy.getMaxTradeAmt(game.diplomacy.get("griffins")) / 10));
@@ -295,9 +298,6 @@ function autoTrade() {
                     else{
                         gamePage.diplomacy.tradeAll(game.diplomacy.get("griffins"));
                     }
-
-                } else if ((mineralsRes.value < (mineralsRes.maxValue * 0.9)) && (ivoryRes.value > mineralsRes.value) && gamePage.diplomacy.get('nagas').unlocked) {
-                    gamePage.diplomacy.tradeAll(game.diplomacy.get("nagas"));
                 } else if (gamePage.diplomacy.get('dragons').unlocked) {
                     gamePage.diplomacy.tradeAll(game.diplomacy.get("dragons"));
                 }
