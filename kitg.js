2cd79994a49a67fe51f0feb9787c2ac937187484
dandcvs <dand73@mail.ru>
2018-08-20 14:44:02 +0400

    add iw philosophy & theology trigger

--- kitg.js
+++ kitg.js
@@ -196,7 +196,7 @@ function autoBuild() {
                         if ((!gamePage.workshop.get("goldOre").researched && btn[i].model.prices.filter(res => res.name == 'science').length > 0) ||
                             (!gamePage.workshop.get("goldOre").researched && gamePage.workshop.get("goldOre").unlocked && btn[i].model.prices.filter(res => res.name == 'minerals').length > 0) ||
                             ((gamePage.bld.buildingsData[27].unlocked && gamePage.bld.buildingsData[27].val == 0 && gamePage.bld.buildingsData[27].name != btn[i].model.metadata.name) && btn[i].model.prices.filter(res => res.name == 'minerals').length > 0) ||
-                            (((!gamePage.science.get('philosophy').researched && gamePage.science.get('philosophy').unlocked)||(!gamePage.science.get('theology').researched && gamePage.science.get('theology').unlocked)) && btn[i].model.prices.filter(res => res.name == 'science').length > 0 && && btn[i].model.prices.filter(res => res.name == 'science')[0].val > 1000)
+                            (((!gamePage.science.get('philosophy').researched && gamePage.science.get('philosophy').unlocked)||(!gamePage.science.get('theology').researched && gamePage.science.get('theology').unlocked)) && btn[i].model.prices.filter(res => res.name == 'science').length > 0 && btn[i].model.prices.filter(res => res.name == 'science')[0].val > 1000)
                         )
                         {}
                         else{
