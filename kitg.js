c92ee3cb61fe63ae87292af4c2b891bf44fb9091
dandcvs <dand73@mail.ru>
2018-08-20 13:27:25 +0400

    add iw philosophy need trigger

--- kitg.js
+++ kitg.js
@@ -196,7 +196,7 @@ function autoBuild() {
                         if ((!gamePage.workshop.get("goldOre").researched && btn[i].model.prices.filter(res => res.name == 'science').length > 0) ||
                             (!gamePage.workshop.get("goldOre").researched && gamePage.workshop.get("goldOre").unlocked && btn[i].model.prices.filter(res => res.name == 'minerals').length > 0) ||
                             ((gamePage.bld.buildingsData[27].unlocked && gamePage.bld.buildingsData[27].val == 0 && gamePage.bld.buildingsData[27].name != btn[i].model.metadata.name) && btn[i].model.prices.filter(res => res.name == 'minerals').length > 0) ||
-                            (!gamePage.science.get('philosophy').researched && btn[i].model.prices.filter(res => res.name == 'science').length > 0)
+                            (!gamePage.science.get('philosophy').researched && !gamePage.science.get('philosophy').unlocked && btn[i].model.prices.filter(res => res.name == 'science').length > 0)
                         )
                         {}
                         else{
