41a6c3ea68a6a44d968669a35651fa71ad187753
dandcvs <dand73@mail.ru>
2018-10-02 14:52:35 +0400

    bf

--- kitg.js
+++ kitg.js
@@ -152,7 +152,7 @@ function autoPraise(){
 	    } else if ((gamePage.resPool.get("faith").value == gamePage.resPool.get("faith").maxValue) && gamePage.tabs[5].rUpgradeButtons.filter(res => res.model.resourceIsLimited == false && (!(res.model.name.includes('(complete)')))).length > 0){
                 var btn = gamePage.tabs[5].rUpgradeButtons;
                 for (var i = 0; i < btn.length; i++) {
-                    if (btn[i].model.enabled && btn[i].model.metadata.unlocked) {
+                    if (btn[i].model.enabled && btn[i].model.visible) {
                         try {
                             btn[i].controller.buyItem(btn[i].model, {}, function(result) {
                                 if (result) {
