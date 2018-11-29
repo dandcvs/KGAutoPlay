be89aea99c5e433349ef4c5c59d7fe807fdccba3
dandcvs <dand73@mail.ru>
2018-11-29 17:39:06 +0400

    bf

--- kitg.js
+++ kitg.js
@@ -380,7 +380,7 @@ function autoCraft2() {
                 ["plate", [["iron",125]],gamePage.ironWill ? 15 :150],
                 ["concrate", [["steel",25],["slab",2500]],0],
                 ["gear", [["steel",15]],0],
-                ["alloy", [["steel",75],["titanium",10]],0],
+                ["alloy", [["steel",75],["titanium",10]],1000],
                 ["eludium", [["unobtainium",1000],["alloy",2500]],1000],
                 ["scaffold", [["beam",50]],0],
                 ["ship", [["scaffold",100],["plate",150],["starchart",25]],100],
