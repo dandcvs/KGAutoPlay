f47eb07f11dd4e459744df053f5122166b5b5e5d
dandcvs <dand73@mail.ru>
2019-02-12 16:43:57 +0400

    bf

--- kitg.js
+++ kitg.js
@@ -105,7 +105,7 @@ function autoPraise(){
 	    if (gamePage.religion.meta[1].meta[5].val == 1) {
             //if (gamePage.religion.getProductionBonus() < 900){
 
-            if (gamePage.religion.getProductionBonus() < getFaithProdCap()){
+            if (gamePage.religion.getProductionBonus() <= getFaithProdCap()){
                 gamePage.religion.praise();
             }
             else if (gamePage.tabs[5].rUpgradeButtons.filter(res => res.model.resourceIsLimited == false && (!(res.model.name.includes('(complete)')))).length > 0){
