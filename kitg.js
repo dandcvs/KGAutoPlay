2aeef78f8ff1228fef8463b22b6661917950c494
dandcvs <dand73@mail.ru>
2019-02-20 16:20:50 +0400

    bf

--- kitg.js
+++ kitg.js
@@ -630,6 +630,7 @@ function autozig() {
                     try {
                          btn[i].controller.buyItem(btn[i].model, {}, function(result) {
                             if (result) {
+                                gamePage.msg('Refine tears: BLS(' + gamePage.resPool.get('sorrow').value  + ')');
                             }
                             });
                     } catch(err) {
