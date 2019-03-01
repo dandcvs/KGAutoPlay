0b7a016b012babaede3b9191de02ba088fb2fe6e
dandcvs <dand73@mail.ru>
2019-03-01 16:19:56 +0400

    bf

--- kitg.js
+++ kitg.js
@@ -419,7 +419,7 @@ function autoCraft2() {
                 ["kerosene", [["oil",7500]],Math.min(gamePage.resPool.get("oil").value/7500*gamePage.getCraftRatio()+1,50000)],
                 ["parchment", [["furs",175]],0],
                 ["manuscript", [["parchment",25],["culture",400]],gamePage.ironWill ? gamePage.resPool.get('culture').value > 1600 ? 50 : 0 : 110],
-                ["compedium", [["manuscript",50],["science",10000]],gamePage.ironWill ?  Math.min(gamePage.resPool.get("science").value/10000*gamePage.getCraftRatio()+1,500) : 110],
+                ["compedium", [["manuscript",50],["science",10000]],gamePage.ironWill ? gamePage.science.get('astronomy').researched ?  Math.min(gamePage.resPool.get("science").value/10000*gamePage.getCraftRatio()+1,500): 0 : 110],
                 ["blueprint", [["compedium",25],["science",25000]],0],
                 ["thorium", [["uranium",250]],Math.min(gamePage.resPool.get("uranium").value/250*gamePage.getCraftRatio()+1,50000)],
                 ["megalith", [["slab",50],["beam",25],["plate",5]],0]
